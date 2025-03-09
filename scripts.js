d4003c16498cb73316d6994888d2a3520cd4e445
dmacauyag <dmacauyag92@gmail.com>
2025-03-08 20:20:10 -0800

    update layout, navbar, and add basic scrolling animation

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-function playAudio(id) {
+const playAudio = id => {
     let audio = document.getElementById(id);
     if (audio.paused) {
         audio.play();
@@ -7,3 +7,26 @@ function playAudio(id) {
         audio.currentTime = 0;
     }
 }
+
+document.getElementById('navbar-toggle').addEventListener('click', () => {
+    document.getElementById('navbar-menu').classList.toggle('active');
+});
+
+document.querySelectorAll('.navbar-menu a').forEach(item => {
+    item.addEventListener('click', event => {
+        event.preventDefault();
+        const targetId = event.target.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+        const offset = 260;
+
+        const elementPosition = targetElement.getBoundingClientRect().top + window.pageYOffset;
+        const offsetPosition = elementPosition - offset;
+
+        window.scrollTo({
+            top: offsetPosition,
+            behavior: 'smooth'
+        });
+
+        document.getElementById('navbar-menu').classList.remove('active');
+    });
+});
